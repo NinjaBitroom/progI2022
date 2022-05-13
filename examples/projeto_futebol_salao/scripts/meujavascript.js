7d3fc59c85bb45a8e0cfa473065b12e25cab3894
Gabriel Bitencourt <gabrielbitercourt@gmail.com>
2022-05-12 22:27:36 -0400

    salvo

--- examples/projeto_futebol_salao/scripts/meujavascript.js
+++ examples/projeto_futebol_salao/scripts/meujavascript.js
@@ -1,14 +1,14 @@
 //alert('funcionou!!!');
+/*
 let t1 = document.getElementById('titulo');
-
 t1.innerHTML = 'Futsal';
 t1.style.border = '2px solid blue';
 t1.style.fontSize = '42pt';
-t1.style.backgroundColor = 'salmon';
+*/
 
 // segunda forma
 if(descricao.innerHTML.length > 100) {
-    alert('Você tem muito texto neste parágrafo!!');
+    //alert('Você tem muito texto neste parágrafo!!');
     descricao.style.color = 'red';
 };
 
@@ -17,7 +17,56 @@ let l1 = document.querySelector('#linha1');
 l1.style.backgroundColor = 'green';
 
 let itens = document.getElementsByTagName('li');
-console.log(itens[8]);
+console.log(itens);
 
 let itens_acoes = document.querySelectorAll('#lista_acoes li');
 console.log(itens_acoes);
+
+let mensagem = '';
+for (let i = 0; i < itens.length; i++) {
+    mensagem += itens[i].innerHTML + '\n';
+};
+
+//alert(mensagem);
+
+mensagem = '';
+itens_acoes.forEach(function(l, i) {
+    mensagem += i + '-' + l.innerHTML + '\n';
+});
+
+//alert(mensagem);
+
+
+let t1 = document.getElementById('titulo');
+texto_ant = t1.innerHTML;
+tam_fonte = t1.style.fontSize;
+
+t1.onmouseover = function() {
+    t1.innerHTML = 'Futsal';
+    t1.style.border = '2px solid blue';
+    t1.style.fontSize = '42pt';
+};
+
+t1.onmouseout = function() {
+    t1.innerHTML = texto_ant;
+    t1.style.border = 'None';
+    t1.style.fontSize = tam_fonte;
+};
+
+ibagem = document.querySelector('#imagem img')
+ibagem.onmouseover = function() {
+    ibagem.src = 'imagens/mqdefault.jpg'
+}
+ibagem.onmouseout = function() {
+    ibagem.src = 'imagens/salao.jfif'
+}
+
+itens_acoes.forEach(function(it) {
+    it.onmouseover = function(e) {
+        e.preventDefault();
+        let v1 = Math.floor(Math.random()*255);
+        let v2 = Math.floor(Math.random()*255);
+        let v3 = Math.floor(Math.random()*255);
+        it.style.backgroundColor = 'rgb(' + v1 + ',' + v2 + ',' + v3 + ')';
+    }
+})
